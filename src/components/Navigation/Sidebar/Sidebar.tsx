00727fb83c2952596d74b171c73a4aa5805de945
GuillermoAguilar <acguiller@gmail.com>
2023-03-11 19:43:48 +0100

    Changes in Sidebar LOGO

--- src/components/Navigation/Sidebar/Sidebar.tsx
+++ src/components/Navigation/Sidebar/Sidebar.tsx
@@ -1,6 +1,20 @@
 import { SideNavigation } from './SideNavigation';
 import { HomeIcon, ArrowLeftIcon } from '@heroicons/react/24/solid';
 import React from 'react';
+import Link from 'next/link';
+
+const Logo = ({ isOpen }: { isOpen: boolean }) => {
+  return (
+    <Link className="flex items-center text-white" href="/">
+      <HomeIcon
+        className={`mr-4 h-6 w-6 flex-shrink-0 text-white ${
+          !isOpen ? 'hidden' : ''
+        }`}
+        aria-hidden="true"
+      />
+    </Link>
+  );
+};
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = React.useState<boolean>(true);
@@ -13,12 +27,7 @@ export const Sidebar = () => {
         <div className={`flex flex-col ${isOpen ? 'w-44' : 'w-22'}`}>
           <div className="flex h-0 flex-1 flex-col">
             <div className="flex h-16 flex-shrink-0 items-center justify-between bg-gray-900 px-4">
-              <HomeIcon
-                className={`mr-4 h-6 w-6 flex-shrink-0 text-white ${
-                  !isOpen ? 'hidden' : ''
-                }`}
-                aria-hidden="true"
-              />
+              <Logo isOpen={isOpen} />
               <ArrowLeftIcon
                 className={`h-6 w-6 flex-shrink-0 text-white transition duration-150 ease-in-out hover:cursor-pointer ${
                   !isOpen ? 'rotate-180' : ''

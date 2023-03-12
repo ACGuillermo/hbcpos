1e06a923032d984e3f2082963cd0a8fda57ebb78
GuillermoAguilar <acguiller@gmail.com>
2023-03-12 18:59:40 +0100

    Update axios client

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,19 +1,6 @@
-import Axios, {
-  type InternalAxiosRequestConfig,
-  type AxiosRequestConfig,
-} from 'axios';
+import Axios, { type InternalAxiosRequestConfig } from 'axios';
 
 import { API_URL, API_TOKEN } from '~/config';
-
-function authRequestInterceptor(config: AxiosRequestConfig) {
-  const token = API_TOKEN;
-  config.headers = config.headers || {};
-  if (token) {
-    config.headers.authorization = `${token}`;
-  }
-  config.headers.Accept = 'application/json';
-  return config;
-}
 
 export const axios = Axios.create({
   baseURL: API_URL,

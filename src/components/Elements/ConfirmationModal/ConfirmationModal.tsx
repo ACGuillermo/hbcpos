f6d1db3d03f17878233ea767d20647508bd6da17
GuillermoAguilar <acguiller@gmail.com>
2023-03-13 08:14:28 +0100

    Change Modal html

--- src/components/Elements/ConfirmationModal/ConfirmationModal.tsx
+++ src/components/Elements/ConfirmationModal/ConfirmationModal.tsx
@@ -46,37 +46,50 @@ export const ConfirmationModal = ({
     <>
       {trigger}
       <Dialog isOpen={isOpen} onClose={close} initialFocus={cancelButtonRef}>
-        <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
-          <div className="sm:flex sm:items-start">
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <DialogTitle
-                as="h3"
-                className="text-lg font-medium leading-6 text-gray-900"
-              >
-                {title}
-              </DialogTitle>
-              <div className="mt-2">
-                {body !== null && (
-                  <p className="text-sm text-gray-500">{body}</p>
-                )}
+        <div className="sm:flex sm:items-start">
+          {icon === 'danger' && (
+            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <ShieldExclamationIcon
+                className="h-6 w-6 text-red-600"
+                aria-hidden="true"
+              />
+            </div>
+          )}
 
-                {children !== null && children}
-              </div>
+          {icon === 'info' && (
+            <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+              <InformationCircleIcon
+                className="h-6 w-6 text-blue-600"
+                aria-hidden="true"
+              />
+            </div>
+          )}
+          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <DialogTitle
+              as="h3"
+              className="text-lg font-medium leading-6 text-gray-900"
+            >
+              {title}
+            </DialogTitle>
+            <div className="mt-2">
+              {body !== null && <p className="text-sm text-gray-500">{body}</p>}
+
+              {children !== null && children}
             </div>
           </div>
+        </div>
 
-          <div className="mt-4 flex justify-end space-x-2">
-            <Button
-              type="button"
-              variant="primary"
-              className="inline-flex w-full justify-center rounded-md border focus:ring-1 focus:ring-indigo-500 focus:ring-offset-1 sm:mt-0 sm:w-auto sm:text-sm"
-              onClick={close}
-              ref={cancelButtonRef}
-            >
-              {cancelButtonText}
-            </Button>
-            {confirmButton}
-          </div>
+        <div className="mt-4 flex justify-end space-x-2">
+          <Button
+            type="button"
+            variant="primary"
+            className="inline-flex w-full justify-center rounded-md border focus:ring-1 focus:ring-indigo-500 focus:ring-offset-1 sm:mt-0 sm:w-auto sm:text-sm"
+            onClick={close}
+            ref={cancelButtonRef}
+          >
+            {cancelButtonText}
+          </Button>
+          {confirmButton}
         </div>
       </Dialog>
     </>
--- src/components/Elements/Dialog/Dialog.tsx
+++ src/components/Elements/Dialog/Dialog.tsx
@@ -24,12 +24,12 @@ export const Dialog: React.FC<DialogProps> = ({
         <UIDialog
           as="div"
           static
-          className="fixed inset-0 z-10 overflow-y-auto"
+          className="fixed inset-0 z-10 flex h-screen min-h-screen items-center justify-center overflow-y-auto"
           open={isOpen}
           onClose={onClose}
           initialFocus={initialFocus}
         >
-          <div className="flex min-h-screen items-end justify-center px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <div className="px-4 pt-4 pb-20 text-center sm:block sm:p-0">
             <Transition.Child
               as={React.Fragment}
               enter="ease-out duration-300"
@@ -42,13 +42,6 @@ export const Dialog: React.FC<DialogProps> = ({
               <UIDialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
             </Transition.Child>
 
-            {/* This element is to trick the browser into centering the modal contents. */}
-            <span
-              className="hidden sm:inline-block sm:h-screen sm:align-middle"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
             <Transition.Child
               as={React.Fragment}
               enter="ease-out duration-300"
@@ -58,7 +51,9 @@ export const Dialog: React.FC<DialogProps> = ({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              {children}
+              <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
+                {children}
+              </div>
             </Transition.Child>
           </div>
         </UIDialog>
